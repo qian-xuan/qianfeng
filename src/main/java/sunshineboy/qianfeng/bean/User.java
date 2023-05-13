38e2994eece41a6d3dc335a9a065761429cdc036
Qianxuan <2392009257@qq.com>
2023-05-13 18:45:19 +0800

    500

--- src/main/java/sunshineboy/qianfeng/bean/User.java
+++ src/main/java/sunshineboy/qianfeng/bean/User.java
@@ -2,14 +2,12 @@ package sunshineboy.qianfeng.bean;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class User {
     private int id;
     private String name;
     private double age;
     private String sex;
-    private List<String> hobby;
+    private String[] hobby;
     private String text;
 }
