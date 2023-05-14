a557dabff5f566d85d81f426164e799cf2ed922f
niltok <huohuomoe@outlook.com>
2023-05-14 08:58:48 +0800

    create runner & db creator

---
+++ createDB.sql
@@ -0,0 +1,15 @@
+drop schema public cascade;
+create schema public;
+grant all on schema public to postgres;
+grant all on schema public to public;
+
+create table tb_user(
+    id int primary key,
+    name text not null,
+    age double precision not null,
+    sex text not null,
+    hobby text[] not null default '{}',
+    "text" text not null
+);
+
+insert into tb_user(id, name, age, sex, hobby, text) VALUES (0, '明日香', 18, '女', '{吃饭, 睡觉, 打真嗣}', '?')
