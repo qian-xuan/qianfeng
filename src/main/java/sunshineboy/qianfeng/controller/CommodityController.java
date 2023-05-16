2b97fd451d8f712d5d6758c615bedd99803609b0
niltok <huohuomoe@outlook.com>
2023-05-16 19:48:49 +0800

    update commodity controller

--- src/main/java/sunshineboy/qianfeng/controller/CommodityController.java
+++ src/main/java/sunshineboy/qianfeng/controller/CommodityController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import sunshineboy.qianfeng.bean.Commodity;
 import sunshineboy.qianfeng.service.CommodityService;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RequestMapping("/commodity")
 public class CommodityController {
     public static final String INDEX = "redirect:/assignmentIndex";
+    public static final String UPDATE = "assignmentUpdate";
     @Autowired
     CommodityService service;
 
@@ -37,5 +39,17 @@ public class CommodityController {
     @RequestMapping("/get")
     public Commodity get(int id) {
         return service.getById(id);
+    }
+
+    @RequestMapping("/add")
+    public String addPage() {
+        return UPDATE;
+    }
+
+    @RequestMapping("/upd")
+    public ModelAndView updPage(int id) {
+        var mv = new ModelAndView(UPDATE);
+        mv.addObject("id", id);
+        return mv;
     }
 }
