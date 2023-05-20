7d2ae98127b48cbdb4740cfa31837e75fb689883
ronaldo728 <10794757+ronaldo728@user.noreply.gitee.com>
2023-05-20 13:30:33 +0800

    submit

--- createDB.sql
+++ createDB.sql
@@ -43,3 +43,10 @@ insert into tb_type(name) values ('女装'),('内衣'),('奢品'),
                                  ('食品'),('生鲜'),('母婴'),
                                  ('医药'),('保健'),('进口');
 
+INSERT INTO public.tb_commodity (id, type, name, price, status, inventory, describe, picture) VALUES (9, 4, '星星板鞋2023新款春季厚底休闲百搭小白鞋原创小众鞋子女', 84, '已上架', 999, '', 'https://g-search3.alicdn.com/img/bao/uploaded/i4/i…1hR5BYgSnVO_!!1065364273.jpg_360x360q90.jpg_.webp');
+INSERT INTO public.tb_commodity (id, type, name, price, status, inventory, describe, picture) VALUES (4, 4, '2023春秋新款单鞋夏季高跟小香风包头凉鞋', 98, '已上架', 666, '', e'https://g-search3.alicdn.com/img/bao/uploaded/i4/i…2KIAWqfLMK5_!!1950009533.jpg_360x360q90.jpg_.webp
+');
+INSERT INTO public.tb_commodity (id, type, name, price, status, inventory, describe, picture) VALUES (8, 4, '卓诗尼百凉鞋女士2023年夏季新款水钻一字带坡跟厚底网红松糕', 199, '已上架', 2222, '', 'https://img.alicdn.com/imgextra/i4/2067420190/O1CN…5ZT6Qy1_!!0-saturn_solar.jpg_360x360q90.jpg_.webp');
+INSERT INTO public.tb_commodity (id, type, name, price, status, inventory, describe, picture) VALUES (10, 4, '华伦V家2023夏季新款尖头黑色铆钉', 169, '已上架', 8888, '', 'https://g-search1.alicdn.com/img/bao/uploaded/i4/i…zE1gypqs_!!2212668881930.jpg_360x360q90.jpg_.webp');
+INSERT INTO public.tb_commodity (id, type, name, price, status, inventory, describe, picture) VALUES (11, 4, '女粗跟圆头一字带成人礼法式婚纱公主表演礼服单', 60, '已上架', 22, null, e'https://g-search3.alicdn.com/img/bao/uploaded/i4/i…a5oZ0Spu_!!2208304973892.jpg_360x360q90.jpg_.webp
+');
