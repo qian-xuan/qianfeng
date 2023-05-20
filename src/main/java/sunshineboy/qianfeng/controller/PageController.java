aac251d6f95bd5e3cc72f0ef22d5cb2efee66976
niltok <huohuomoe@outlook.com>
2023-05-20 16:39:34 +0800

    add type page

--- src/main/java/sunshineboy/qianfeng/controller/PageController.java
+++ src/main/java/sunshineboy/qianfeng/controller/PageController.java
@@ -1,7 +1,9 @@
 package sunshineboy.qianfeng.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import sunshineboy.qianfeng.service.CommodityService;
@@ -18,6 +20,14 @@ public class PageController {
         var mv = new ModelAndView("/index");
         var commodities = commodityService.list();
         mv.addObject("commodities", commodities);
+        mv.addObject("typeMap", typeService.map());
+        return mv;
+    }
+
+    @RequestMapping("/type/{tid}")
+    public ModelAndView typePage(@PathVariable("tid") int tid) {
+        var mv = new ModelAndView("/index");
+        mv.addObject("commodities", commodityService.query().eq("type", tid).list());
         mv.addObject("typeMap", typeService.map());
         return mv;
     }
