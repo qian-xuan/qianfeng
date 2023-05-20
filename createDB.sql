e67e81bdb6113dbd23e1d2d10ce1a15b214dae9f
motong <1282848931@qq.com>
2023-05-20 10:22:06 +0800

    create table_Type

--- createDB.sql
+++ createDB.sql
@@ -24,4 +24,18 @@ create table tb_commodity(
     picture text
 );
 
-insert into tb_user(name, password, age, sex, hobby, text) VALUES ('明日香', '0',18, '女', '{吃饭, 睡觉, 打真嗣}', '?')
+create table tb_type(
+    id serial primary key,
+    name text not null
+);
+
+insert into tb_user(name, password, age, sex, hobby, text) VALUES ('明日香', '0',18, '女', '{吃饭, 睡觉, 打真嗣}', '?');
+
+insert into tb_type(name) values ('女装'),('内衣'),('奢品'),
+                                 ('女鞋'),('男鞋'),('箱包'),
+                                 ('美妆'),('饰品'),('洗护'),
+                                 ('男装'),('运动'),('百货'),
+                                 ('手机'),('数码'),('企业礼品'),
+                                 ('家装'),('电器'),('车品'),
+                                 ('食品'),('生鲜'),('母婴'),
+                                 ('医药'),('保健'),('进口');
